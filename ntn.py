2b6603f12ba72b52ada7c55c984f0d471feb5355
Brandon James <brandon@brandonsjames.com>
2019-06-27 23:25:09 -0500

    additional work on dns module

--- ntn.py
+++ ntn.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def dns_check():
     if request.method == 'POST':
-        return render_template('index.html', dns_results = ntn_dns(request.form['dns_lookup']))
+        return render_template('index.html', 
+                        dns_results = ntn_dns(request.form['dns_lookup'],
+                        request.form['record_type'])
+                    )
     return render_template('index.html')
--- ntn_dns.py
+++ ntn_dns.py
@@ -2,7 +2,7 @@ import dns.resolver
 
 _RESOLVER_LIST = ['8.8.8.8', '4.2.2.2', '1.1.1.1']
 
-def ntn_dns(hostname):
+def ntn_dns(hostname, record_type):
 
     results = []
 
@@ -10,10 +10,40 @@ def ntn_dns(hostname):
 
         dns.resolver.override_system_resolver(resolver)
 
-        response = dns.resolver.Resolver().query(hostname, 'A')
+        try:
+
+            response = dns.resolver.Resolver().query(hostname, record_type)
+
+        except dns.resolver.NoAnswer:
+
+            response = 'No Answer'
 
         results.append({'response': response[:], 'resolver': resolver})
 
     dns.resolver.restore_system_resolver()
 
     return results
+
+if __name__ == "__main__":
+
+    test_request = [
+        {'hostname': 'brandonsjames.com', 'record_type': 'A'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'AAAA'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'CNAME'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'DNAME'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'MX'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'NS'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'PTR'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'SOA'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'SRV'},
+        {'hostname': 'brandonsjames.com', 'record_type': 'TXT'},
+    ]
+
+    for request in test_request:
+
+        results = ntn_dns(request['hostname'], request['record_type'])
+
+        for result in results:
+
+            print(result['response'])
+            print(result['resolver'])
