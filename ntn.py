bded1d77ec13ea9f4bb4d7a3a3067643b20d7925
Brandon James <brandon@brandonsjames.com>
2019-07-01 22:45:28 -0500

    added scrollback and turned the site into a single page app

--- ntn.py
+++ ntn.py
@@ -6,28 +6,28 @@ from ntn_subnet import ntn_subnet
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
-@app.route('/dnslookup', methods=['GET', 'POST'])
+@app.route('/dns', methods=['GET', 'POST'])
 def dns_check():
     if request.method == 'POST':
         return render_template('dns.html', 
                         dns_results = ntn_dns(request.form['dns_lookup'],request.form['record_type']),
                     dns_lookup = request.form['dns_lookup'],record_type = request.form['record_type'], dns_active=True)
-    return render_template('dns.html', dns_active=True)
+    return render_template('dns_app.html', dns_active=True)
 
 @app.route('/curl', methods=['GET', 'POST'])
 def curl():
     if request.method == 'POST':
         return render_template('curl.html', results = ntn_curl(request.form['url']),
-                        url = request.form['url'], curl_active=True)
-    return render_template('curl.html', curl_active=True)
+                        url = request.form['url'])
+    return render_template('curl_app.html')
 
 @app.route('/subnet', methods=['GET', 'POST'])
 def subnet():
     if request.method == 'POST':
         return render_template('subnet.html', results = ntn_subnet(request.form['ip_address'], request.form['subnet_mask']),
                         ip_address = request.form['ip_address'], subnet_mask = request.form['subnet_mask'], subnet_active=True)
-    return render_template('subnet.html', subnet_active=True)
+    return render_template('subnet_app.html', subnet_active=True)
---
+++ static/load_elements.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    $('#curl_nav').click(function(){
+        $('#app').load('curl #app', function() {
+        });
+    })
+    $('#app').submit(function(event){
+
+        event.preventDefault()
+
+        var curr_app = document.getElementById("app").firstChild
+
+        classes = curr_app.classList
+
+        if(classes.contains("curl")){
+
+            $.ajax({
+                url: 'curl',
+                type: 'post',
+                data: $('[name=url]'),
+                success: function(data) {
+
+                    $("#term").append(data);
+
+                    var term = document.getElementById("term");
+                    term.scrollTop = term.scrollHeight;            
+                }
+            });
+
+        }else if (classes.contains("dns")){
+
+            $.ajax({
+                url: 'dns',
+                type: 'post',
+                data: {dns_lookup: $('[name=dns_lookup]').val(), record_type: $('[name=record_type').val()},
+                success: function(data) {
+
+                    $("#term").append(data);
+
+                    var term = document.getElementById("term");
+                    term.scrollTop = term.scrollHeight;            
+                }
+            });
+
+        }else if (classes.contains("subnet")){
+
+            $.ajax({
+                url: 'subnet',
+                type: 'post',
+                data: {ip_address: $('[name=ip_address]').val(), subnet_mask: $('[name=subnet_mask').val()},
+                success: function(data) {
+
+                    $("#term").append(data);
+
+                    var term = document.getElementById("term");
+                    term.scrollTop = term.scrollHeight;            
+                }
+            });
+
+        }
+
+       return false;
+    
+    })
+
+    $('#dns_nav').click(function(){
+        $('#app').load('dns #app', function() {
+        });
+    })
+    $('#subnet_nav').click(function(){
+        $('#app').load('subnet #app', function() {
+        });
+    })
+});
