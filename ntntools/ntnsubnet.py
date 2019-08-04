025fa123b860b971bca66496081b9910c2f07256
Brandon James <brandon@brandonsjames.com>
2019-08-03 21:06:23 -0500

    removed address type from results

--- ntntools/ntnsubnet.py
+++ ntntools/ntnsubnet.py
@@ -1,5 +1,6 @@
 import ipaddress
 
+
 def subnet(ip_address, subnet_mask):
 
     try:
@@ -17,82 +18,6 @@ def subnet(ip_address, subnet_mask):
 
         return {'Error': 'Invalid subnet mask or IP address'}
 
-    address_type = ''
-
-    try:
-
-        if subnet.is_global:
-
-            address_type += 'Public '
-
-    except AttributeError:
-
-            pass
-
-    try:
-
-        if subnet.is_private:
-
-            address_type += 'Private '
-        
-    except AttributeError:
-
-            pass
-
-    try:
-
-        if subnet.is_multicast:
-
-            address_type += 'Multicast '
-        
-    except AttributeError:
-
-        pass
-
-    try:
-
-        if subnet.is_link_local:
-
-            address_type += 'Link Local '
-
-    except AttributeError:
-
-        pass
-
-    try:
-        
-        if subnet.is_site_local:
-
-            address_type += 'Site Local '
-
-    except AttributeError:
-
-        pass
-
-    try:
-
-        if subnet.is_loopback:
-
-            address_type += 'Loopback '
-
-    except AttributeError:
-
-        pass
-
-    try:
-
-        if subnet.is_reserved:
-
-            address_type += 'Reserved '
-
-    except AttributeError:
-
-        pass
-
-    if address_type == '':
-
-        address_type = "Undefined (Range may span private, public and/or multicast address space)"
-
 
     if subnet.num_addresses == 1:
 
@@ -102,7 +27,6 @@ def subnet(ip_address, subnet_mask):
             'CIDR': '/{}'.format(subnet.prefixlen),
             'Wildcard Bits': subnet.hostmask,
             'Number of Addresses': subnet.num_addresses,
-            'Address Type': address_type
         }
         
     elif subnet.num_addresses == 2:
@@ -114,7 +38,6 @@ def subnet(ip_address, subnet_mask):
             'Number of Addresses': subnet.num_addresses,
             'First Host': subnet.network_address,
             'Last Host': subnet.broadcast_address,
-            'Address Type': address_type
         }
 
     else:
@@ -128,5 +51,4 @@ def subnet(ip_address, subnet_mask):
             'Number of Addresses': subnet.num_addresses,
             'First Host': subnet.network_address + 1,
             'Last Host': subnet.broadcast_address - 1,
-            'Address Type': address_type
         }
